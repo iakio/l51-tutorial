1e305961a7608839e990b38ff159461b205e8d31
ISHIDA Akio <iakio.monospacenet@gmail.com>
2015-11-15 22:18:34 +0900

    Flashメッセージ追加

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Flash;
 
 class Authenticate
 {
@@ -38,6 +39,7 @@ class Authenticate
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
+                Flash::warning('Please sign in.');
                 return redirect()->guest('auth/login');
             }
         }
