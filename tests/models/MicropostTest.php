8bbbb7026c273d7cdff5d91ca8c4bd249b6e1d19
ISHIDA Akio <iakio.monospacenet@gmail.com>
2016-01-04 22:15:50 +0900

    Micropostのテストの修正
    Carbonの加減算メソッドはImmutableではなく破壊的な変更をするので
    インスタンスをコピーするよう修正。

--- tests/models/MicropostTest.php
+++ tests/models/MicropostTest.php
@@ -15,8 +15,8 @@ class MicropostTest extends TestCase
         /** @var App\User $user */
         $user = factory(User::class)->create();
         $now = Carbon::now();
-        $older_micropost = $user->microposts()->create(['content' => 'Lorem ipsum', 'created_at' => $now->subDay(1)])->toArray();
-        $newer_micropost = $user->microposts()->create(['content' => 'Lorem ipsum', 'created_at' => $now->subHour(1)])->toArray();
+        $older_micropost = $user->microposts()->create(['content' => 'Lorem ipsum', 'created_at' => $now->copy()->subDay(1)])->toArray();
+        $newer_micropost = $user->microposts()->create(['content' => 'Lorem ipsum', 'created_at' => $now->copy()->subHour(1)])->toArray();
         $microposts = $user->microposts->toArray();
         $this->assertEquals([ $newer_micropost, $older_micropost ], $microposts);
 
